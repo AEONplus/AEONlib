d0e66bcf4c3211422e7575629fe315292fdc1420
Christian Hettlage <hettlage@saao.ac.za>
2025-09-12 17:52:16 +0200

    Add validators for greater than and less than relations

--- src/aeonlib/salt/validators.py
+++ src/aeonlib/salt/validators.py
@@ -5,9 +5,21 @@ from typing import Any
 from pydantic import AfterValidator
 
 
+def _check_gt(a: Any, b: Any) -> Any:
+    if a <= b:
+        raise ValueError(f"{a} is not greater than to {b}.")
+    return a
+
+
 def _check_ge(a: Any, b: Any) -> Any:
     if a < b:
         raise ValueError(f"{a} is not greater than or equal to {b}.")
+    return a
+
+
+def _check_lt(a: Any, b: Any) -> None:
+    if a >= b:
+        raise ValueError(f"{a} is not less than to {b}.")
     return a
 
 
@@ -15,6 +27,36 @@ def _check_le(a: Any, b: Any) -> None:
     if a > b:
         raise ValueError(f"{a} is not less than or equal to {b}.")
     return a
+
+
+def GreaterThan(value: Any):
+    """
+    Return a Pydantic validator for checking a greater than relation.
+
+    The returned validator can be used in a type annotation::
+
+        import pydantic
+
+        class DummyModel(pydantic.BaseModel):
+            duration: Annotated[float, GreaterThan(4)]
+
+    Pydantic will first perform its own internal validation and then check whether
+    the field value is greater than the argument passed to `GreaterThan` (4 in the
+    example above).
+
+    It is up to the user to ensure that the field value and the argument of
+    `GreaterThan` can be compared.
+
+    Parameters
+    ----------
+    value
+       Value against which to compare.
+
+    Returns
+    -------
+    A validator for checking a greater than relation.
+    """
+    return AfterValidator(lambda v: _check_gt(v, value))
 
 
 def GreaterEqual(value: Any):
@@ -28,9 +70,9 @@ def GreaterEqual(value: Any):
         class DummyModel(pydantic.BaseModel):
             duration: Annotated[float, GreaterEqual(4)]
 
-    Pydantic will then first perform its own internal validation and then check
-    whether the field value is greater than or equal to the argument passed to
-    `GreaterEqual` (4 in the example above).
+    Pydantic will first perform its own internal validation and then check whether
+    the field value is greater than or equal to the argument passed to `GreaterEqual`
+    (4 in the example above).
 
     It is up to the user to ensure that the field value and the argument of
     `GreaterEqual` can be compared.
@@ -47,6 +89,36 @@ def GreaterEqual(value: Any):
     return AfterValidator(lambda v: _check_ge(v, value))
 
 
+def LessThan(value: Any):
+    """
+    Return a Pydantic validator for checking a less than relation.
+
+    The returned validator can be used in a type annotation::
+
+        import pydantic
+
+        class DummyModel(pydantic.BaseModel):
+            height: Annotated[float, LessThan(4)]
+
+    Pydantic will first perform its own internal validation and then check whether
+    the field value is less than or equal to the argument passed to `LessEqual` (4 in
+    the example above).
+
+    It is up to the user to ensure that the field value and the argument of
+    `LessThan` can be compared.
+
+    Parameters
+    ----------
+    value
+       Value against which to compare.
+
+    Returns
+    -------
+    A validator for checking a less than relation.
+    """
+    return AfterValidator(lambda v: _check_lt(v, value))
+
+
 def LessEqual(value: Any):
     """
     Return a Pydantic validator for checking a less than or equal to relation.
@@ -58,9 +130,9 @@ def LessEqual(value: Any):
         class DummyModel(pydantic.BaseModel):
             height: Annotated[float, LessEqual(4)]
 
-    Pydantic will then first perform its own internal validation and then check
-    whether the field value is less than or equal to the argument passed to
-    `LessEqual` (4 in the example above).
+    Pydantic will first perform its own internal validation and then check whether
+    the field value is less than or equal to the argument passed to `LessEqual` (4 in
+    the example above).
 
     It is up to the user to ensure that the field value and the argument of
     `LessEqual` can be compared.
--- tests/salt/test_validators.py
+++ tests/salt/test_validators.py
@@ -4,11 +4,19 @@ from typing import Annotated
 import pytest
 from pydantic import BaseModel, ValidationError
 
-from aeonlib.salt.validators import GreaterEqual, LessEqual
+from aeonlib.salt.validators import GreaterEqual, GreaterThan, LessEqual, LessThan
+
+
+class GreaterThanModel(BaseModel):
+    a: Annotated[int, GreaterThan(4)]
 
 
 class GreaterEqualModel(BaseModel):
     a: Annotated[int, GreaterEqual(4)]
+
+
+class LessThanModel(BaseModel):
+    a: Annotated[int, LessThan(4)]
 
 
 class LessEqualModel(BaseModel):
@@ -18,9 +26,27 @@ class LessEqualModel(BaseModel):
 class TestValidators:
     @pytest.mark.parametrize(
         "a, expectation",
+        [
+            (3, pytest.raises(ValidationError)),
+            (4, pytest.raises(ValidationError)),
+            (5, nullcontext()),
+        ],
+    )
+    def test_greater_than(self, a, expectation):
+        """Test that the GreaterThan validator validates correctly."""
+        with expectation:
+            GreaterThanModel(a=a)
+
+    def test_greater_than_does_not_change_field_value(self):
+        """Test that the field value is not changed by the GreaterThan validator."""
+        assert GreaterThanModel(a=7).a == 7
+
+    @pytest.mark.parametrize(
+        "a, expectation",
         [(3, pytest.raises(ValidationError)), (4, nullcontext()), (5, nullcontext())],
     )
     def test_greater_equal(self, a, expectation):
+        """Test that the GreaterEqual validator validates correctly."""
         with expectation:
             GreaterEqualModel(a=a)
 
@@ -30,9 +56,27 @@ class TestValidators:
 
     @pytest.mark.parametrize(
         "a, expectation",
+        [
+            (3, nullcontext()),
+            (4, pytest.raises(ValidationError)),
+            (5, pytest.raises(ValidationError)),
+        ],
+    )
+    def test_less_than(self, a, expectation):
+        """Test that the LessThan validator validates correctly."""
+        with expectation:
+            LessThanModel(a=a)
+
+    def test_less_than_does_not_change_field_value(self):
+        """Test that the field value is not changed by the LessThan validator."""
+        assert LessThanModel(a=2).a == 2
+
+    @pytest.mark.parametrize(
+        "a, expectation",
         [(3, nullcontext()), (4, nullcontext()), (5, pytest.raises(ValidationError))],
     )
     def test_less_equal(self, a, expectation):
+        """Test that the LessEqual validator validates correctly."""
         with expectation:
             LessEqualModel(a=a)
 
