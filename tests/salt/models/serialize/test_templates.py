6060dd73b3f5eac585228541a9be4372397cc8ab
Christian Hettlage <hettlage@saao.ac.za>
2026-02-06 18:04:53 +0200

    Add templates for serializing Salticam instances

---
+++ tests/salt/models/serialize/test_templates.py
@@ -0,0 +1,35 @@
+import pytest
+
+from aeonlib.salt.models.util import render_template, validate_xml
+
+
+def test_salticam_detector_template(base_salticam_detector):
+    """Test that the Salticam detector template generates valid XML."""
+    xml = render_template(
+        "salticam_detector.xml", detector=base_salticam_detector.model_dump()
+    )
+    validate_xml(xml)
+    assert True
+
+
+@pytest.mark.parametrize("full", [False, True])
+def test_salticam_template(full: bool, base_salticam, base_salticam_dither_pattern):
+    salticam = base_salticam.model_dump()
+    salticam["filter_sequence"].append({"filter": "Cousins R", "exposure_time": 42})
+    if full:
+        salticam["dither_pattern"] = base_salticam_dither_pattern.model_dump()
+        salticam["include_flat"] = True
+    else:
+        salticam["dither_pattern"] = None
+        salticam["include_flat"] = False
+    xml = render_template("salticam.xml", salticam=salticam)
+
+    if full:
+        assert "Dither" in xml
+        assert "Flat" in xml
+    else:
+        assert "Dither" not in xml
+        assert "Flat" not in xml
+
+    validate_xml(xml)
+    assert True
