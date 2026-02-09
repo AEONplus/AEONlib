744176c2aea081c712a14b6defe16039af15ae3e
Christian Hettlage <hettlage@saao.ac.za>
2026-02-09 14:12:17 +0200

    Handle RSS filters correctly when generating XML

--- tests/salt/models/serialize/test_templates.py
+++ tests/salt/models/serialize/test_templates.py
@@ -4,6 +4,7 @@ import astropy.units as u
 
 from aeonlib.salt.models import SalticamFilterSequenceStep
 from aeonlib.salt.models.util import render_template, validate_xml
+from tests.salt.models.conftest import base_rss_longslit_spectroscopy
 
 
 def test_salticam_detector_template(base_salticam_detector):
@@ -63,6 +64,7 @@ def test_salticam_detector_template(full: bool, base_rss_detector):
 
 @pytest.mark.parametrize("full", [False, True])
 def test_rss_imaging(full: bool, base_rss_imaging, base_rss_polarimetry):
+    """Tests that the RSS imaging template generates valid XML."""
     configuration = base_rss_imaging
 
     if full:
@@ -76,3 +78,48 @@ def test_rss_imaging(full: bool, base_rss_imaging, base_rss_polarimetry):
         assert "BeamsplitterOrientation" in xml
     else:
         assert "BeamsplitterOrientation" not in xml
+
+    validate_xml(xml)
+    assert True
+
+
+@pytest.mark.parametrize(
+    "filter_name, expected_element",
+    [("pi04340", "FilterId"), ("Johnson V", "SalticamFilter")],
+)
+def test_rss_imaging_filter(filter_name: str, expected_element: str, base_rss_imaging):
+    """Tests that RSS imaging filters are handled correctly when generating XML."""
+    configuration = base_rss_imaging
+    configuration.filter = filter_name
+
+    xml = render_template("rss_imaging.xml", configuration=configuration)
+    assert expected_element in xml
+
+    validate_xml(xml)
+    assert True
+
+
+@pytest.mark.parametrize("full", [False, True])
+def test_rss_longslit_spectroscopy(
+    full: bool, base_rss_longslit_spectroscopy, base_rss_polarimetry
+):
+    configuration = base_rss_longslit_spectroscopy
+
+    if full:
+        configuration.polarimetry = base_rss_polarimetry
+    else:
+        configuration.polarimetry = None
+
+    xml = render_template(
+        "rss_spectroscopy.xml", configuration=configuration.model_dump()
+    )
+
+    assert "PredefinedMask" in xml
+
+    if full:
+        assert "BeamsplitterOrientation" in xml
+    else:
+        assert "BeamsplitterOrientation" not in xml
+
+    validate_xml(xml)
+    assert True
