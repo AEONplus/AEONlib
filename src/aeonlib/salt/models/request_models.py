632ecd9737bc2088ee92eee15903dbd90647208f
Christian Hettlage <hettlage@saao.ac.za>
2026-02-13 13:12:04 +0200

    Add a method for getting the set of attachments used by a request

--- src/aeonlib/salt/models/request_models.py
+++ src/aeonlib/salt/models/request_models.py
@@ -22,3 +22,32 @@ class Request(BaseModel, validate_assignment=True):
     proposal_code: str
 
     blocks: Annotated[list, MinLen(1)]
+
+    def attachments(self) -> set[pathlib.Path]:
+        """
+        Return the set of attachments for this request.
+
+        All the attachment paths are reso,lved using the `resolve` method of the
+        `pathlib.Path` class.
+
+        Returns
+        -------
+        The set of attachments.
+        """
+        _attachments: set[pathlib.Path] = set()
+
+        for block in self.blocks:
+            for finder_chart in block.acquisition.finder_charts:
+                if not isinstance(finder_chart, pathlib.Path):
+                    raise ValueError("The finder chart value is not a Path instance.")
+                _attachments.add(finder_chart)
+
+            if block.instrument.instrument_name == "RSS":
+                if hasattr(block.instrument.configuration, "mask"):
+                    mask = block.instrument.configuration.mask
+                    if not isinstance(mask, pathlib.Path):
+                        raise ValueError("The mask value is not a Path instance.")
+                    _attachments.add(mask)
+
+        # Remove duplicates
+        return set(a.resolve() for a in _attachments)
--- tests/salt/models/test_request_models.py
+++ tests/salt/models/test_request_models.py
@@ -1,3 +1,5 @@
+import pathlib
+
 import pytest
 from pydantic import ValidationError
 
@@ -15,3 +17,88 @@ class TestRequest:
             Request(proposal_code="2025-1-SCI-042", blocks=[])
         assert exc_info.value.errors()[0]["loc"] == ("blocks",)
         assert exc_info.value.errors()[0]["type"] == "too_short"
+
+    def test_no_attachments(
+        self, base_request, base_block, base_rss, base_rss_longslit_spectroscopy
+    ):
+        """Test the case that the request includes no attachments."""
+        request = base_request
+        block = base_block
+        rss = base_rss
+        configuration = base_rss_longslit_spectroscopy
+        block.acquisition.finder_charts = []
+        rss.configuration = configuration
+        block.instrument = rss
+        request.blocks = [block]
+
+        assert request.attachments() == set()
+
+    def test_multiple_attachments(
+        self, base_request, base_block, base_rss, base_rss_multi_object_spectroscopy
+    ):
+        """Test the case that the request includes multiple attachments."""
+        finder_chart_1 = (
+            pathlib.Path(__file__).parent.parent / "data" / "dummy_finder_chart.pdf"
+        )
+        finder_chart_2 = (
+            pathlib.Path(__file__).parent.parent / "data" / "dummy_finder_chart_2.pdf"
+        )
+        mos_mask = (
+            pathlib.Path(__file__).parent.parent / "data" / "dummy_rss_mos_mask.rsim"
+        )
+        request = base_request
+        block = base_block
+        rss = base_rss
+        configuration = base_rss_multi_object_spectroscopy
+        configuration.mask = mos_mask
+        block.acquisition.finder_charts = [finder_chart_1, finder_chart_2]
+        rss.configuration = configuration
+        block.instrument = rss
+        request.blocks = [block]
+
+        assert request.attachments() == {
+            finder_chart_1.resolve(),
+            finder_chart_2.resolve(),
+            mos_mask.resolve(),
+        }
+
+    def test_duplicate_attachments(
+        self, base_request, base_block, base_rss, base_rss_multi_object_spectroscopy
+    ):
+        """Test the case that the request uses the same attachment multiple times."""
+        # finder_chart_1a, finder_chart_1b and finder_chart_1c denote the sane file
+        finder_chart_1a = (
+            pathlib.Path(__file__).parent.parent / "data" / "dummy_finder_chart.pdf"
+        )
+        finder_chart_1b = (
+            pathlib.Path(__file__).parent.parent
+            / "data/../data"
+            / "dummy_finder_chart.pdf"
+        )
+        finder_chart_1c = (
+            pathlib.Path(__file__).parent.parent
+            / "data/../../salt/data"
+            / "dummy_finder_chart.pdf"
+        )
+        finder_chart_2 = (
+            pathlib.Path(__file__).parent.parent / "data" / "dummy_finder_chart_2.pdf"
+        )
+        mos_mask = finder_chart_1c  # as we are testing for duplicates
+        request = base_request
+        block = base_block
+        rss = base_rss
+        configuration = base_rss_multi_object_spectroscopy
+        configuration.mask = mos_mask
+        block.acquisition.finder_charts = [
+            finder_chart_1a,
+            finder_chart_2,
+            finder_chart_1b,
+        ]
+        rss.configuration = configuration
+        block.instrument = rss
+        request.blocks = [block]
+
+        assert request.attachments() == {
+            finder_chart_1a.resolve(),
+            finder_chart_2.resolve(),
+        }
